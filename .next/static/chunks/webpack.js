f1963d05e069149740e7584625a2a3deec3313ea
Vimlan G <gvimlan95@gmail.com>
2021-07-14 21:13:25 +0800

    port changed to 8080

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8423c963b0e0a8cb5e08";
+/******/ 	var hotCurrentHash = "d3b47203f980a128f835";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
